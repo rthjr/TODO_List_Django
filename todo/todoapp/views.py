b136902b57a08f906304e1efb289d4836ba50495
Potato <137735702+rthjr@users.noreply.github.com>
2024-05-04 16:19:59 +0700

    test

---
+++ todo/todoapp/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# Create your views here.
+
+def home(request):
+    return render(request,'todoapp/todoo.html', {})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if len(password) < 3:
+            messages.error(request, 'Password must be at least 3 characters')
+            return redirect('register')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Error, username already exists.')
+            return redirect('register')
+
+            
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
+
+
+        messages.success(request,'successfully created')
+        return redirect('login')
+    return render(request,'todoapp/register.html', {})
+
+def loginpage(request):
+    if request.method == 'POST':
+        username = request.POST.get('uname')
+        password = request.POST.get('pass')
+
+        validate_user = authenticate(username=username, password=password)
+        if validate_user is not None:
+            login(request, validate_user)
+            return redirect('home_page')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
+        
+
+
+    return render(request,'todoapp/login.html', {})
+
+
+def logoutpage(request):
+    return redirect('home_page')
